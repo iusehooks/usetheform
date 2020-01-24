c4afae2269f7a2078dd6b933506b961d5758d935
Antonio <a.pangallo85@gmail.com>
2020-01-24 17:27:58 +0100

    docs: logo usetheform

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,10 @@
 export default {
   base: "/usetheform",
   ignore: ["README.md", "LICENSE.md"],
-  menu: ["Introduction", "Components", "Hooks"]
+  menu: ["Introduction", "Components", "Hooks"],
+  themesDir: "./docs",
+  themeConfig: {
+    showDarkModeSwitch: false
+  },
+  public: "./docs/gatsby-theme-docz/assets"
 };
