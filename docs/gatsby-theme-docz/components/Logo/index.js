1bf10291d32183ba87a724907494e19b294a36bb
Antonio Pangallo <apangallo@ebay.com>
2020-07-07 17:53:07 +0200

    DOCZ_STYLES docz styles changes

--- docs/gatsby-theme-docz/components/Logo/index.js
+++ docs/gatsby-theme-docz/components/Logo/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./../../assets/logo.png";
+import "./../../style.css";
 
 export const Logo = () => (
   <img style={{ height: "40px", width: "auto" }} src={logo} alt="usetheform" />
