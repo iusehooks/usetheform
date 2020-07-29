2766b951de3f9b90cf7a38df4b61cae5d46a41fc
Antonio <a.pangallo85@gmail.com>
2020-07-29 18:56:18 +0200

    improve FormContext performances

--- src/FormContext.js
+++ src/FormContext.js
@@ -1,33 +1,42 @@
-import React from "react";
-
+import React, { useMemo, memo } from "react";
 import { useForm } from "./hooks/useForm";
 import { ContextForm as Context } from "./hooks/useOwnContext";
 
-export function FormContext({
-  children,
-  initialState,
-  onChange,
-  onInit,
-  onReset,
-  onSubmit,
-  reducers,
-  _getInitilaStateForm_, // Private API
-  _onMultipleForm_, // Private API
-  name,
-  action
-}) {
-  const ctx = useForm({
+export const FormContext = memo(
+  ({
+    children,
     initialState,
     onChange,
     onInit,
     onReset,
     onSubmit,
     reducers,
-    _getInitilaStateForm_,
-    _onMultipleForm_,
+    _getInitilaStateForm_, // Private API
+    _onMultipleForm_, // Private API
     name,
     action
-  });
+  }) => {
+    const props = useForm({
+      initialState,
+      onChange,
+      onInit,
+      onReset,
+      onSubmit,
+      reducers,
+      _getInitilaStateForm_,
+      _onMultipleForm_,
+      name,
+      action
+    });
 
-  return <Context.Provider value={ctx}>{children}</Context.Provider>;
-}
+    const ctx = useMemo(() => props, [
+      props.state,
+      props.isValid,
+      props.status,
+      props.pristine,
+      props.submitted
+    ]);
+
+    return <Context.Provider value={ctx}>{children}</Context.Provider>;
+  }
+);
