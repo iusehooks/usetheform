44e62edc320d718bb37da7414845a4f962dc13f0
Antonio <a.pangallo85@gmail.com>
2020-10-02 13:13:22 +0200

    removing unused code

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ export { Select } from "./Select";
 export { TextArea } from "./TextArea";
 export { Collection } from "./Collection";
 export { FormContext } from "./FormContext";
-
 export { createForm, getValueByPath, STATUS } from "./utils/formUtils";
-export { toFormData } from "./utils/toFormData";
-
 export { usePublicContextForm as useForm } from "./hooks/useOwnContext";
 export { useValidation } from "./hooks/useValidation";
 export { useAsyncValidation } from "./hooks/useAsyncValidation";
